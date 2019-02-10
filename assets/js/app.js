71790a5e472d38504f70fb00bb08b5fa20546084
Mathieu <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-02-10 12:09:58 +0100

    add react

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,5 +15,3 @@ require('../css/header.scss')
 require('../css/movie-detail.scss')
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
