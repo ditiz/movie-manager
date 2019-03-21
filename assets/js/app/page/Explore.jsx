06a93df4fb86819a59ba2aab9f28263ed5939ecf
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-03-21 22:51:04 +0100

    add state and api function in explore component

--- assets/js/app/page/Explore.jsx
+++ assets/js/app/page/Explore.jsx
@@ -2,8 +2,25 @@ import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 
 const Explore = React.memo(function Explore() {
+
+	const [ready, setReady] = React.useState(false)
+	const [movies, setMovies] = React.useState([])
+
+	const useApi = () => {
+		//url temporaire
+		const url = '/api/movies/search/scrubs'
+
+		fetch(url)
+		.then(res => res.json)
+		.then(res => {
+			setMovies(res)
+			setReady(true)
+		}).catch(err => alert(err.reason))
+	}
+
+
 	return (
-		<div>
+		<div onLoad={useApi()}>
 			Explore
 		</div>
 	)
